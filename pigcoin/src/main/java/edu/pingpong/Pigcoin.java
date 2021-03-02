48360f076c8bf1e0dd333e0322cb549cdbc69e5b
BertaVR <bverges@cifpfbmoll.eu>
2021-03-02 19:33:06 +0100

    build: Configuración proyecto

---
+++ pigcoin/src/main/java/edu/pingpong/Pigcoin.java
@@ -0,0 +1,13 @@
+package edu.pingpong;
+
+/**
+ * Hello world!
+ *
+ */
+public class Pigcoin
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
